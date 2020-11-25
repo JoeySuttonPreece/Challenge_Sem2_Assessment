db4af266c2aa934358710c39ea82cc7a408f6805
JoeySuttonPreece <102568393@student.swin.edu.au>
2020-11-26 09:50:50 +1100

    fill environment with config on firebase app page

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDZr7cck8EzL0O-dhGOL5sy_1ed5to7ou4",
+    authDomain: "challenge-sem2-assessment.firebaseapp.com",
+    databaseURL: "https://challenge-sem2-assessment.firebaseio.com",
+    projectId: "challenge-sem2-assessment",
+    storageBucket: "challenge-sem2-assessment.appspot.com",
+    messagingSenderId: "426200599028",
+    appId: "1:426200599028:web:3ce41370bc6155adc591a1"
+  }
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+import { environment as common } from "./environment.common";
+
 export const environment = {
-  production: true
+  ...common,
+  production: true,
 };
